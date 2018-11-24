856187a30b2b3278b95d7aa7af599510b9ad6761
Will Lewis <will@wflewis.com>
2018-11-24 13:16:01 -0600

    Pass tests

--- spec/divclass_spec.cr
+++ spec/divclass_spec.cr
@@ -1,9 +1,27 @@
 require "./spec_helper"
 
 describe Divclass do
-  # TODO: Write tests
+  test "that opening tags work" do
+    assert Divclass.translate(%[<card>]) == %[<div class="card">]
+  end
 
-  test "that it works" do
-    assert 0 == 1
+  test "that closing tags work" do
+    assert Divclass.translate(%[</card>]) == %[</div>]
+  end
+
+  test "that opening tags with class work" do
+    assert Divclass.translate(%[<card class="text-center">]) == %[<div class="card text-center">]
+  end
+
+  test "that normal opening tags work" do
+    assert Divclass.translate(%[<div>]) == %[<div>]
+  end
+
+  test "that normal closing tags work" do
+    assert Divclass.translate(%[</div>]) == %[</div>]
+  end
+
+  test "that normal opening tags with class work" do
+    assert Divclass.translate(%[<div class="text-center">]) == %[<div class="text-center">]
   end
 end
--- src/divclass.cr
+++ src/divclass.cr
@@ -1,3 +1,33 @@
+require "xml"
+
 module Divclass
-  VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
+  VERSION     = {{ `shards version #{__DIR__}`.chomp.stringify }}
+  IGNORE_TAGS = %w(div)
+
+  def self.translate(input : String) : String
+    input.gsub(/\<.*\>/) do |match|
+      list = XML.parse(match).children
+
+      if list.size == 0
+        "</div>"
+      else
+        node = list[0]
+        name = node.name
+        return match if IGNORE_TAGS.includes?(name)
+
+        old_class = node["class"]?
+
+        new_class =
+          if old_class
+            [name, old_class].join(" ")
+          else
+            name
+          end
+
+        %[<div class="#{new_class}">]
+      end
+    end
+  end
 end
+
+puts Divclass.translate("<card>")
